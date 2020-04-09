bdabd0002a6abf2197e2bfd22a117c89288bdffd
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-09 23:36:51 +0530

    added rout

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ server.get('/about.html', (req, res) => {
   //handler similar to http server module
   res.send('in about page'); // to send string send method
 });
+server.get('/contact.html', (req, res) => {
+  //handler similar to http server module
+  res.send('in contact page'); // to send string send method
+});
 
 server.listen(config.port, () => {
   //listner function
