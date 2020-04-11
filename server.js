a872cb933dcbf1dadd54ab7bd847ba3367cdd1f0
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-11 22:37:18 +0530

    added comments about templates to be used with server, express

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import config from './config';
 import apiRouter from './api/index';
 import express from 'express';
+// instead of express we could have used framework KOA which has generalised middleware on top of nodejs
+//hapi.js
+//sails.js ( on ruby on rails)
+// for templating engine pug uses simplifies indentation represented tags 
+// for templating Handlebars
 const server = express();
 
 //route
 server.get('/', (req, res) => {
   res.render('index',{
-    content: 'ejs template!'
+    content: 'ejs template!<h1>H1</h1>'
   });
   //handler similar to http server module
   // res.send('hello from server'); // to send string send method
