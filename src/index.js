a1806d608860f5faecb6356692494309f34f5d15
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-11 23:20:32 +0530

    added react src

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDom from "react-dom";
 
 ReactDom.render(
-  React.createElement(
-    'h1', null, 'hello React'
-  ),
+  React.createElement('h1', null, 'hello React'),
   document.getElementById('root')
 );
